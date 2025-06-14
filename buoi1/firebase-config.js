4bbb073da4915d043b2a9ab809d0a23b2eda2a32
HuyNguyen0722 <134412520+HuyNguyen0722@users.noreply.github.com>
2025-06-14 19:21:10 +0700

    Initial commit

---
+++ buoi1/firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAUttGhxt-Xb85JcA5X_53YGAgDS7UWUGk",
+  authDomain: "fir-1-c3a7a.firebaseapp.com",
+  projectId: "fir-1-c3a7a",
+  storageBucket: "fir-1-c3a7a.firebasestorage.app",
+  messagingSenderId: "226036856726",
+  appId: "1:226036856726:web:3ec605752958e17ca98fb7",
+  measurementId: "G-N7VMFLG8XQ"
+};
